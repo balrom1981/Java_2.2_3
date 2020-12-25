77bceb039ce7fb87325cc23163a1ada572dde310
balrom1981 <balrom@bk.ru>
2020-12-25 11:08:41 +0300

    Formated

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,9 @@
 public class CreditPaymentService {
     public double calculate(int amount, int month) {
-        double annual_percentage = 9.99;
-        double monthly_percentage = annual_percentage / 100 / 12;
-        double payment = amount * (monthly_percentage + monthly_percentage / (Math.pow((1 + monthly_percentage) , month) - 1));
+        double annualPercentage = 9.99;
+//        Расчет аннуитетного платежа
+        double monthlyPercentage = annualPercentage / 100 / 12;
+        double payment = amount * (monthlyPercentage + monthlyPercentage / (Math.pow((1 + monthlyPercentage) , month) - 1));
         return payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double payment_one = service.calculate(1_000_000, 12);
-        System.out.println(payment_one);
-        double payment_two = service.calculate(1_000_000, 24);
-        System.out.println(payment_two);
-        double payment_three = service.calculate(1_000_000, 36);
-        System.out.println(payment_three);
+        double paymentOne = service.calculate(1_000_000, 12);
+        System.out.println(paymentOne);
+        double paymentTwo = service.calculate(1_000_000, 24);
+        System.out.println(paymentTwo);
+        double paymentThree = service.calculate(1_000_000, 36);
+        System.out.println(paymentThree);
     }
 }
